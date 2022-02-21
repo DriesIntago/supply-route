dbd53fa293e8937f7bae68779e9ed06c8a0d23df
DriesDrofmans <dries@intago.be>
2022-02-21 16:57:55 +0100

    Add map

---
+++ resources/js/components/Map.vue
@@ -0,0 +1,55 @@
+
+<script>
+import {
+  LMap,
+  LIcon,
+  LTileLayer,
+  LMarker,
+  LControlLayers,
+  LTooltip,
+  LPopup,
+  LPolyline,
+  LPolygon,
+  LRectangle,
+} from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
+export default {
+  components: {
+    LMap,
+    LIcon,
+    LTileLayer,
+    LMarker,
+    LControlLayers,
+    LTooltip,
+    LPopup,
+    LPolyline,
+    LPolygon,
+    LRectangle,
+  },
+  data() {
+    return {
+      zoom: 12,
+    };
+  },
+};
+</script>
+
+<template>
+  <div id="map">
+    <l-map
+      v-model="zoom"
+      v-model:zoom="zoom"
+      :center="[51.2364327, 4.4198728]"
+    >
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<style>
+  #map {
+    height: 75vh;
+  }
+</style>
